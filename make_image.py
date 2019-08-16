0f14d764e12718aa26189775da07bcf4993d62a2
mikuriya <mickly9@hotmail.com>
2019-08-16 11:31:25 +0900

    use join

--- make_image.py
+++ make_image.py
@@ -1,14 +1,15 @@
 import cv2
 import sys
 
-size = (48, 48)
+width = 48
+height = 48
 image_t = 'uint16_t'
 print('#include <avr/pgmspace.h>')
 print('#ifndef IMG_WIDTH')
-print('#define IMG_WIDTH', size[1])
+print('#define IMG_WIDTH', width)
 print('#endif')
 print('#ifndef IMG_HEIGHT')
-print('#define IMG_HEIGHT', size[0])
+print('#define IMG_HEIGHT', height)
 print('#endif')
 print('typedef %s image_t;' % image_t)
 for i in range(1, len(sys.argv)):
@@ -16,18 +17,21 @@ for i in range(1, len(sys.argv)):
     if img is None:
         print('failed to read image', file=sys.stderr)
         exit(1)
-    img = cv2.resize(img, size)
+    img = cv2.resize(img, (height, width))
     print('// %s' % sys.argv[i])
     print('const image_t image%d[IMG_WIDTH * IMG_HEIGHT] PROGMEM = {' % i)
+    rows = []
     for x in range(img.shape[1]):
+        cols = []
         for y in range(img.shape[0]):
             a = img[y][x]
             c = ((a[2] & 0b11111000) << 8) + ((a[1] & 0b11111100) << 3) + (
                 (a[0] & 0b11111000) >> 3)
-            print('/* %2d-%d */ 0x%04X' % (x, y, c), end=', ')
-        print()
+            cols.append('/* %2d-%d */ 0x%04X' % (x, y, c))
+        rows.append('  ' + ', '.join(cols))
+    print(',\n'.join(rows))
     print('};')
 
 print('#define COUNT_OF_IMAGES %d' % (len(sys.argv) - 1))
-print('const image_t* images[COUNT_OF_IMAGES] = {', ', '.join(sys.argv[1:]),
-      '};')
+print('const image_t* images[COUNT_OF_IMAGES] = {',
+      ', '.join(map(lambda i: 'image%d' % i, range(1, len(sys.argv)))), '};')
